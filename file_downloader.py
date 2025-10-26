5b348b5b05f0a0514e52194062e1979ea68e30e9
Priteenanda Das <priteenanda2005@email.com>
2025-10-26 09:59:14 +0530

    Add gdown for Google Drive download

--- file_downloader.py
+++ file_downloader.py
@@ -1,12 +1,8 @@
 import os
-import requests
+import gdown
 
 def download_similarity_file():
-    url = "https://drive.google.com/uc?export=download&id=1mWnd_QMj4OJuI0d1RAhNbtSObpGJ5FJw"
-    local_path = "similarity.pkl"
-    if not os.path.exists(local_path):
-        print("Downloading similarity.pkl...")
-        response = requests.get(url)
-        with open(local_path, "wb") as f:
-            f.write(response.content)
-        print("Download complete.")
+    url = "https://drive.google.com/uc?id=1mWnd_QMj4OJuI0d1RAhNbtSObpGJ5FJw"
+    output = "similarity.pkl"
+    if not os.path.exists(output):
+        gdown.download(url, output, quiet=False)
